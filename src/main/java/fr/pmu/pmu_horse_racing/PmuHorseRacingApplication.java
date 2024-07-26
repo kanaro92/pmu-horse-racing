940bb72e5f5235c0dff6f898f1ffe6b1be219fba
Hamath Kane <kawkumputer@gmail.com>
2024-07-26 02:22:00 +0200

    Bootstraping project.

---
+++ src/main/java/fr/pmu/pmu_horse_racing/PmuHorseRacingApplication.java
@@ -0,0 +1,13 @@
+package fr.pmu.pmu_horse_racing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmuHorseRacingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmuHorseRacingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pmu/pmu_horse_racing/PmuHorseRacingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pmu.pmu_horse_racing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmuHorseRacingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
